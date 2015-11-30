9ce1fe9b334037488edf2f133cbdc15c9f8bb8bb
Pierre Rolland <prolland@lafourchette.com>
2015-11-30 12:01:45 +0100

    Added doc

--- utils.h
+++ utils.h
@@ -1,7 +1,14 @@
 #ifndef __COLOG_UTILS__
 #define __COLOG_UTILS__
 
+/**
+ * Replaces all occurences of "rep" with "with" in "orig"
+ */
 char *str_replace(char* orig, char* rep, char* with);
+
+/**
+ * Returns 1 if "str" was found in "array", 0 otherwise
+ */
 int  in_array(const char* str, char** array, int length);
 
 #endif /* __COLOG_UTILS__ */
